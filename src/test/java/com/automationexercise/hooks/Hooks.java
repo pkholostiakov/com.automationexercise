84eb0650c80e872c6f1719d905f05dd2290e6120
pavel <pkholostiakov@gmail.com>
2023-02-26 18:07:47 -0800

    Add the driver

---
+++ src/test/java/com/automationexercise/hooks/Hooks.java
@@ -0,0 +1,41 @@
+package com.automationexercise.hooks;
+
+import java.io.File;
+import java.io.IOException;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import static com.automationexercise.utils.ConfigReader.*;
+import com.automationexercise.utils.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+
+public class Hooks {
+	
+	private WebDriver driver;
+	
+	@Before
+	public void setUp() {
+		driver = Driver.getDriver();
+		driver.get(getProperty("url"));
+	}
+	
+	@After
+	public void tearDown(Scenario scenario) {
+		if (scenario.isFailed()) {
+			String screenshotName = scenario.getName().replaceAll(" ", "_");
+			String screenshotPath = "target/screenshots" + screenshotName + ".png";
+			try {
+				byte[] screenshot = ((TakesScreenshot)Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+				scenario.attach(screenshot, "image/png", "Screenshot");
+				FileUtils.copyFile(((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.FILE),
+						new File(screenshotPath));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		Driver.quitDriver();
+	}
+}
---
+++ src/test/java/com/automationexercise/utils/Utils.java
@@ -0,0 +1,5 @@
+package com.automationexercise.utils;
+
+public class Utils {
+
+}
